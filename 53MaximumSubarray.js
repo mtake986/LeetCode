c102b0e8674ad2bf3ecb0b04feae313b536c22ed
Masahiro Takechi <tak21011@byui.edu>
2022-03-09 15:32:48 -0700

    Create 53MaximumSubarray.js

---
+++ 53MaximumSubarray.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// https://leetcode.com/problems/maximum-subarray/
+// Test cases are fine, but failed running when submitting. 
+// O(n!) 
+var maxSubArray = function(nums) {
+    let maxNum = nums[0]
+    const arrayLen = nums.length
+    let sumOfSubarray = 0;
+    let lenOfSubarray = 1;
+    let maxSumSubarray = []
+    while (lenOfSubarray <= arrayLen) {
+        let startIndex = 0
+        while (startIndex + lenOfSubarray <= arrayLen) {
+            const subarray = nums.slice(startIndex, startIndex+lenOfSubarray)
+            sumOfSubarray = subarray.reduce(((total, num) => total + num), 0)
+            if (maxNum < sumOfSubarray) {
+                maxNum = sumOfSubarray;
+                maxSumSubarray = subarray
+            }
+            startIndex++;
+        }
+
+        lenOfSubarray++;
+    }
+    return maxNum
+};
+
