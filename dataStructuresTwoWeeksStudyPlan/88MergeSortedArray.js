e6d34a28fe7ac07499fb17c41cadb314aa4fe5fc
Masahiro Takechi <tak21011@byui.edu>
2022-03-10 21:48:31 -0700

    Update 88MergeSortedArray.js

--- dataStructuresTwoWeeksStudyPlan/88MergeSortedArray.js
+++ dataStructuresTwoWeeksStudyPlan/88MergeSortedArray.js
@@ -31,3 +31,20 @@ var merge = function(nums1, m, nums2, n) {
     }
 };
 // i know it's slow but i didn't come up with an opyimal solution.
+
+var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m, nums1.length - m);
+    
+    var i = 0;
+    var j = 0;
+    
+    while (j < nums2.length) {
+        if (nums1[i] === undefined || nums1[i] > nums2[j]) {
+            nums1.splice(i, 0, nums2[j]);
+            j++;
+            i++;
+        } else {
+            i++;
+        }
+    }
+};
